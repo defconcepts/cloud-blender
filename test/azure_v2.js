2575d17ff09db6700cc0e46a72ef0d61e7a64d19
Omer Arbel <omer.arbel@hp.com>
2015-11-08 18:29:43 +0200

    azure v2

---
+++ test/azure_v2.js
@@ -0,0 +1,73 @@
+var should = require('should'),
+   underscore = require('underscore'),
+   compute = require('../lib/azure_v2.js'),
+   azureConfig = require('../examples/azure_v2.json');
+
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+compute.setProxy('http://web-proxy.isr.hp.com:8080');
+
+var providerName = 'azure_v2',
+   regionAuthSettings = azureConfig,
+   regionLimits = {};
+
+
+var regionContext = compute.createRegionContext(regionAuthSettings, regionLimits);
+
+
+describe('checking azure local atomic lib', function () {
+
+
+   it('should create an authentication context', function () {
+
+      var regionContext = compute.createRegionContext(regionAuthSettings, regionLimits);
+      should.exist(regionContext.cloudRegion);
+      should.exist(regionContext.providerName);
+      should.exist(regionContext.groupId);
+      should.exist(regionContext.tenantId);
+      should.exist(regionContext.clientId);
+      should.exist(regionContext.secret);
+   });
+});
+
+describe('checking azure atomic lib', function () {
+
+   it('should launch instance on azure', function (done) {
+      var regionContext = compute.createRegionContext( regionAuthSettings, regionLimits),
+
+         settingsCreate = {
+            regionContext: regionContext,
+            nodeParams: {
+               imageId: '',
+               instanceType: 'Standard_D3',
+               tags: {
+                  jobId: 'dummyJobId',
+                  env: 'test',
+                  role: 'injector-Test'
+               },
+               userData: {'paramA': 'keyA', 'paramB': 'keyB', 'paramC': 'keyc'}
+            }
+         };
+
+
+      this.timeout(300000);
+
+            compute.createNode(settingsCreate, null, 0, function (error1, result1) {
+
+               if (error1) {
+                  console.log('error creating node1-' + error1);
+                  done();
+                  return;
+               }
+
+
+               should.not.exist(error1);
+
+
+
+
+            });
+         });
+      });
+
